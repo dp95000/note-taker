11803b1180459888d71b862bf930d97a657a2759
dp <davepollan@gmail.com>
2020-02-11 00:06:15 -0500

    api call for notes working

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 const port = 3000;
 
+const dbFilePath = path.resolve(__dirname, '..', 'db', 'db.json');
+
+// this lines insures that all elements in the 'public' folder are served. This is needed to insure CSS & JS assests are served to pages other than the root page.
 app.use(express.static('public'));
 
 // for main page
@@ -12,16 +16,18 @@ app.get('/',(_, res) => {
     res.sendfile(filePath);
 });
 
-// for notes page
+// for displaying notes page
 app.get('/notes',(_, res) => {
     const filePath = path.resolve(__dirname, '..', 'public', 'notes.html')
     res.sendfile(filePath);
 });
 
-// for loading assets - CSS & JS
-app.get('/assets/js/index.js',(_, res) => {
-    const filePath = path.resolve(__dirname, '..', 'public', 'notes.html')
-    res.sendfile(filePath);
+// for notes page
+app.get('/api/notes', async (_, res) => {
+    const fileData = await fs.readFile(dbFilePath, 'utf-8');
+    const data = JSON.parse(fileData);
+    res.json(data);
+    console.log(fileData);
 });
 
 
@@ -31,6 +37,7 @@ app.get('*',(_, res) => {
     res.sendfile(filePath);
 });
 
+// test message
 app.listen(port, () => {
     console.log(`Test application listening on port ${port}!`)
 });
